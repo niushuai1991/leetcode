97ea9ea28f6b02c96b2f0bfbcad2e579ebee3bdb
niushuai <guisu2010@gmail.com>
2019-03-11 00:41:05 +0800

    mininum Number of Arrows to Burst Balloons

--- 452.py
+++ 452.py
@@ -2,16 +2,17 @@ class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
         if not points:
             return 0
+        # 1.排序
         points = sorted(points)
-        # 拿第一个气球的结束点
+        # 2.找到第一个点的开始点和结束点
         end = points[0][1]
         num = 1
-        for s, e in points:
+        # 3.遍历所有的点
+        for s,e in points[1:]:
+            # 如果结束点大于新气球的开始点
             if end >= s:
-                # 说明这一轮使用的箭还能射破这个气球
                 end = min(end, e)
             else:
-                # 说明需要增加一把新的箭来使用
                 num+=1
                 end = e
         return num
--- README.md
+++ README.md
@@ -11,7 +11,7 @@ https://leetcode.com/problems/subarrays-with-k-different-integers/
 
 2月25日【优达每日一题】有 n 个数组，从 n 个数组中各选一个数，使他们的目标和为目标值，是否有合法解(median)
 
-2月26日 （未完成）
+2月26日
 https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/
 
 2月27日 （未完成）
